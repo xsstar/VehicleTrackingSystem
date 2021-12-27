0e3d24bda155b62becff822b5efa73e3fa69e3f7
havva polatlı <74872560+xsstar@users.noreply.github.com>
2021-12-27 17:09:02 +0300

    all methods for vehicles

--- app/controllers/vehicleController.js
+++ app/controllers/vehicleController.js
@@ -15,7 +15,48 @@ const createVehicle = async (req, res) => {
   res.status(201).json(`${data.vehicle_plate} successfully created..`);
 };
 
-module.exports = {
-  createVehicle
-}
+const getVehicles = (req, res) => {
+  pg_client.query(
+    "SELECT * FROM vehicles ORDER BY id ASC",
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows);
+    }
+  );
+};
 
+const updateVehicle = (req, res) => {
+  const id = req.params.id;
+  pg_client.query(
+    "UPDATE vehicles SET vehicle_plate = $1, current_status = $2, is_active = $3 where id=$4",
+    [req.body.vehicle_plate, req.body.current_status, req.body.is_active, id],
+
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send(`Vehicle with id-${id} successfully updated..`);
+    }
+  );
+};
+
+const deleteVehicle = async (req, res) => {
+  const id = req.params.id;
+  await pg_client.query(
+    "DELETE FROM vehicles WHERE id = $1",
+    [id],
+    (error, results) => {
+      if (error) throw error;
+      res.status(200).send(`Vehicle with id-${id}  successfully deleted..`);
+    }
+  );
+};
+
+module.exports = {
+  createVehicle,
+  getVehicles,
+  updateVehicle,
+  deleteVehicle,
+};
--- app/routes/vehicleRoute.js
+++ app/routes/vehicleRoute.js
@@ -4,8 +4,8 @@ const vehicleController = require("../controllers/vehicleController");
 const router = express.Router();
 
 router.route("/vehicle_add").post(vehicleController.createVehicle);
-/* router.route("/vehicle_list").get(vehicleController.getVehicles);
+router.route("/vehicle_list").get(vehicleController.getVehicles);
 router.route("/vehicle_update/:id").patch(vehicleController.updateVehicle);
-router.route("/vehicle_delete/:id").delete(vehicleController.deleteVehicle); */
+router.route("/vehicle_delete/:id").delete(vehicleController.deleteVehicle);
 
 module.exports = router;
